4d77938f016000700acb7d8208caae0eaf089a0b
shengbangshenghua <w@shengbangshenghua.com>
2018-10-11 09:29:49 +0800

    no message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-/**
- * Created by lenovo on 18/10/11 011.
- */
+var vm = new Vue({
+    el:"#app",
+    data:{
+        news:[
+            {"title":"新闻标题","content":"新闻的具体内容","sort":"军事","name":"新华网","time":"时间戳","update":"更新时间戳","state":false},
+            {"title":"新闻标题","content":"新闻的具体内容","sort":"科技","name":"新华网","time":"时间戳","update":"更新时间戳","state":true},
+            {"title":"新闻标题","content":"新闻的具体内容","sort":"军事","name":"新华网","time":"时间戳","update":"更新时间戳","state":true}
+
+        ]
+    }
+})
